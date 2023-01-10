b690d303c66181e784c32cd84101bb6ab8ede700
haliljon <110017001+haliljon@users.noreply.github.com>
2023-01-10 18:00:43 +0500

    Add header and footer to the project

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
+  mode: 'production',
   entry: './src/index.js',
   devServer: {
     static: './dist',
@@ -13,11 +13,37 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      { test: /\.(png|svg|jpeg|jpg|gif)$/i, type: 'asset/resource' },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        include: path.resolve(
+          __dirname,
+          './node_modules/bootstrap-icons/font/fonts',
+        ),
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'webfonts',
+            publicPath: '../webfonts',
+          },
+        },
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html"),
+      template: path.resolve(__dirname, 'src', 'index.html'),
     }),
   ],
   output: {
