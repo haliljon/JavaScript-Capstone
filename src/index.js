62f4261f6a67f0a7d98e64957c546dbb32ed31f1
haliljon <110017001+haliljon@users.noreply.github.com>
2023-01-11 18:36:04 +0500

    made some design changes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'boxicons';
 
+import displayCard from './modules/displayCard.js';
+
 require('bootstrap-icons/font/bootstrap-icons.css');
 
-import displayCard from './modules/displayCard.js';
 const URL = 'https://api.disneyapi.dev/characters';
 
 const getData = async () => {
