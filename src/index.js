c582f6c9c1a37712e837f227fee28199e685b15e
haliljon <110017001+haliljon@users.noreply.github.com>
2023-01-11 13:05:45 +0500

    Add cards from API

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'boxicons';
 
 require('bootstrap-icons/font/bootstrap-icons.css');
+
+import displayCard from './modules/displayCard.js';
+const URL = 'https://api.disneyapi.dev/characters';
+
+const getData = async () => {
+  const res = await fetch(URL);
+  const data = await res.json();
+  const information = data.data;
+  displayCard(information);
+};
+
+getData();
