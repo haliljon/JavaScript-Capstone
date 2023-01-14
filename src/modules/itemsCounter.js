071a0ec8fce1180a21e91b03ae8ccc0e5bd59f9a
haliljon <110017001+haliljon@users.noreply.github.com>
2023-01-14 14:18:45 +0500

    created test for items counter function

--- src/modules/itemsCounter.js
+++ src/modules/itemsCounter.js
@@ -1,9 +1,2 @@
-const itemsCounter = (count) => {
-  const ul = document.querySelector('#ul');
-  const home = document.querySelector('#home');
-  const list = document.createElement('li');
-  list.innerHTML = `<a class="nav-link scrollto active" href="#hero">Number of cards in this page: ${count}</a>`;
-  ul.insertBefore(list, home);
-};
-
-export default itemsCounter;
+const itemsCounter = (item) => item.length;
+module.exports = itemsCounter;
--- src/test/countComments.test.js
+++ src/test/countComments.test.js
@@ -2,21 +2,20 @@
  * @jest-environment jsdom
  */
 
-import countComments from "../modules/countComments";
+import countComments from '../modules/countComments';
 
 describe('Count comments', () => {
-    beforeAll(() => {
-        document.body.innerHTML = ` <ul class="ul-comment">
+  beforeAll(() => {
+    document.body.innerHTML = ` <ul class="ul-comment">
         <li>1</li>
         <li>3</li>
-        <li>5
-        </li>
-       </ul>`
-    });
+        <li>5</li>
+       </ul>`;
+  });
 
-    test('it should print 3', () => {
-        const ul = document.querySelector('.ul-comment');
-        const result = countComments(ul);
-        expect(result).toBe(3);
-    });
-})
+  test('it should print 3', () => {
+    const ul = document.querySelector('.ul-comment');
+    const result = countComments(ul);
+    expect(result).toBe(3);
+  });
+});
